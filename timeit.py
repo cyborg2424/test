24fb3123bcb1e27aad52a9e43eccf23cc26c5cfa
cyborg2424 <coolbao2410@gmail.com>
2022-05-15 15:00:19 -0700

    test all

--- timeit.py
+++ timeit.py
@@ -1,19 +1,17 @@
 import time
 
-
 def calculate_time(func):
     def wrapper():
         start = time.time()
         func()
         end = time.time()
-        total = end - start
-        print("The time is", total)
-    return wrapper()
-
+        total_time = end - start
+        print(f'Total time {total_time}')
+        return func
+    return wrapper
 
 @calculate_time
-def total_time():
+def test_time():
     time.sleep(5)
 
-
-total_time()
+test_time()
